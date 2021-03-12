1a170d00cd526bf9b08e8f084939cb2aaeb3fa74
Diego Lipez <codegomain@gmail.com>
2021-03-12 13:27:37 -0500

    Created inicial window Access Control

---
+++ app.py
@@ -0,0 +1,8 @@
+from ui import WinAccess
+
+def main():
+    winpass = WinAccess()
+    winpass.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ ui.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+class WinAccess(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('SJE Docs')
+
+        # Main Container
+        frame = tk.LabelFrame(self, text = ' Control de Acceso ')
+        frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+        
+        # User Input
+        tk.Label(frame, text = ' Usuario: ').grid(row = 0, column = 0)
+        self.userInput = tk.Entry(frame, width = 16)
+        self.userInput.focus()
+        self.userInput.grid(row = 0, column = 1, pady = 5)
+
+        # Password Input
+        tk.Label(frame, text = ' Contraseña: ').grid(row = 1, column = 0)
+        self.userPass = tk.Entry(frame, width = 16, show = '*')
+        self.userPass.grid(row = 1, column = 1, pady = 5)
+
+        # Button Acept & Cancel
+        tk.Button(frame, text = 'Aceptar').grid(row = 2, column = 1, padx = 5, pady = 5)
+        tk.Button(frame, text = 'Cancelar').grid(row = 2, column = 2, padx = 5, pady = 5)
